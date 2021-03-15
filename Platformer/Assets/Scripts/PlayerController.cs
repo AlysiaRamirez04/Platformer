ec5f4cef4ea569f5abe69c410ac3f61613e1e02e
Alysia_Ramirez04 <aly.ram.044@knights.ucf.edu>
2021-03-14 23:28:10 -0400

    Challenge commit

--- Platformer/Assets/Scripts/PlayerController.cs
+++ Platformer/Assets/Scripts/PlayerController.cs
@@ -9,18 +9,85 @@ public class PlayerController : MonoBehaviour
 
     public float speed;
 
-    public Text score;
+    public Text scoreText;
+    public Text winText;
+    public Text livesText;
+    public Text loseText;
 
-    private int scoreValue = 0;
+    private int scoreValue;
+    private int livesValue;
+
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        scoreValue = 0;
+        winText.text = "";
+        loseText.text = "";
+        livesValue = 3;
+        livesText.text = "Lives: " + livesValue.ToString();
+        scoreText.text = "Score: " + scoreValue.ToString ();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
+    void Update()
+    {
+
+     if (Input.GetKeyDown(KeyCode.W))
+
+        {
+
+          anim.SetInteger("State", 3);
+
+         }
+
+     if (Input.GetKeyUp(KeyCode.W))
+
+        {
+
+          anim.SetInteger("State", 0);
+
+         }
+
+     if (Input.GetKeyDown(KeyCode.D))
+
+        {
+
+          anim.SetInteger("State", 2);
+
+         }
+
+     if (Input.GetKeyUp(KeyCode.D))
+
+        {
+
+          anim.SetInteger("State", 0);
+
+         }
+
+    if (Input.GetKeyDown(KeyCode.A))
+
+        {
+
+          anim.SetInteger("State", 2);
+
+         }
+
+     if (Input.GetKeyUp(KeyCode.A))
+
+        {
+
+          anim.SetInteger("State", 0);
+
+         }
+
+
+    }
+
+
     void FixedUpdate()
     {
         float hozMovement = Input.GetAxis("Horizontal");
@@ -28,16 +95,40 @@ public class PlayerController : MonoBehaviour
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
     }
 
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
        if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
+            scoreText.text = "Score: " + scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+             if (scoreValue >= 8)
+             {
+                 winText.text = "You Win! Game Created by Alysia Ramirez";
+             }
+             if (scoreValue == 4) 
+            {
+             transform.position = new Vector3(40.0f, 0.0f, 0.0f); 
+             livesValue = 3;
+             livesText.text = "Lives: " + livesValue.ToString();
+            }
+        }
+
+        if (collision.collider.tag == "Enemy")
+        {
+            livesValue -= 1;
+            livesText.text = "Lives: " + livesValue.ToString();
+            Destroy(collision.collider.gameObject);
+            if (livesValue <= 0)
+             {
+                 loseText.text = "You Lose!";
+                 Destroy(gameObject);
+             }
         }
 
     }
+
 
     private void OnCollisionStay2D(Collision2D collision)
     {
